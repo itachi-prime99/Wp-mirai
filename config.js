1e8aac9d5430504ac4f0b30920fa8f94acdeeb54
itachi-prime99 <ovhi.bbz108@outlook.com>
2025-04-21 09:37:24 +0600

    Create config.json

---
+++ config.js
@@ -0,0 +1,12 @@
+{
+  "botName": "Eren",
+  "ownerName": "itachi-prime99",
+  "ownerNumber": "8801839268235",
+  "adminNumbers": ["8801839268235"],
+  "prefix": ".",
+  "language": "bn",
+  "autoRead": true,
+  "autoTyping": true,
+  "replyType": "mirai",
+  "helpImage": "https://i.imgur.com/QSAgPa0.jpeg"
+}
